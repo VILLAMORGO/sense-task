2e1fda1a64660f3760ef34edacafd78a31833e01
VILLAMOR GO <villamorgo@gmail.com>
2022-02-25 23:11:47 +0800

    Added destroy button to categ and task

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+    validates :title, presence: true
+    validates :title, uniqueness: true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :category
+  validates :title, presence: true
+  validates :title, uniqueness: { scope: :category_id}
 end
