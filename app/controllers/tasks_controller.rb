b53219441ea92d485797a35419443935fdd4cea8
VILLAMOR GO <villamorgo@gmail.com>
2022-03-03 17:44:04 +0800

    Added tasks sorting by category feat

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,31 +1,39 @@
 class TasksController < ApplicationController
   before_action :authenticate_account!
-  before_action :set_category
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   # GET /tasks
   def index
-    @tasks = @category.tasks
+    @categories = current_account.categories
+    category_id = params[:category_id]
+
+    if !category_id.nil?
+      @tasks = Task.where(:category_id => category_id)
+    else
+      @tasks = current_account.tasks
+    end
+    
   end
 
   # GET /tasks/1
   def show
-    @tasks = @category.tasks
+    @tasks = current_account.tasks
   end
 
   # GET /tasks/new
   def new
-    @task = @category.tasks.build
+    @task = current_account.tasks.build
   end
 
   # GET /tasks/1/edit
   def edit
-    @tasks = @category.tasks
+    @tasks = current_account.tasks
   end
 
   # POST /tasks
   def create
-    @task = @category.tasks.build(task_params)
+    @task = Task.new(task_params)
+    
 
     if @task.save
       redirect_to @task, notice: 'Task was successfully created.'
@@ -49,19 +57,20 @@ class TasksController < ApplicationController
     redirect_to tasks_url, notice: 'Task was successfully destroyed.'
   end
 
+  def update_status
+    @tasks = current_account.tasks.find(params[:id])
+    @tasks.update(status: params[:status])
+    redirect_to :tasks, notice: "#{@tasks.title} changed status to #{@tasks.status}"
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_task
-      @task = @category.tasks.find(params[:id])
+      @task = current_account.tasks.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:title, :body, :category_id)
+      params.require(:task).permit(:title, :body, :date, :category_id)
     end
-
-    def set_category
-      @category = current_account.categories.find_by(params[:category_id])
-    end
-    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :tasks
   devise_for :accounts
+  resources :tasks do
+    member do
+      patch :update_status
+    end
+  end
   resources :categories
+  
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
