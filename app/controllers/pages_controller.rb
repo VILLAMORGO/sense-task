24ad11a57298f85ecb04bd4bdb3c4422e0c80691
VILLAMOR GO <villamorgo@gmail.com>
2022-03-04 18:42:09 +0800

    final styling for homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
 
     def home
+        @tasks_count = current_account.tasks.count
         @tasks_overdue = current_account.tasks.where(status: ["Not started", "In progress"]).where.not(date: [Date.today.all_day, nil]).order("created_at DESC")
         @tasks_today = current_account.tasks.where(date: Date.today.all_day).order("created_at DESC")
         @tasks_completed = current_account.tasks.where(status: ["Completed"]).where.not(date: [nil]).order("created_at DESC")
